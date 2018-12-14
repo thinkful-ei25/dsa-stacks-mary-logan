3aabb9fc3266a152bb795d3d7d06cdf5ea5d02c4
Logan Wang <loganxwang@hotmail.com>
2018-12-14 12:30:42 -0800

    finished parantheses

--- stackdrills.js
+++ stackdrills.js
@@ -1,12 +1,15 @@
 'use strict';
 const Stack = require('./stack');
 
-// function peek(stack) {
-//   console.log(stack.top);
-// }
-// function display(stack) {
-//   console.log(JSON.stringify(stack));
-// }
+function peek(stack) {
+  if (!stack.top) {
+    return null;
+  }
+  return stack.top;
+}
+function display(stack) {
+  console.log(JSON.stringify(stack));
+}
 
 // const starTrek = new Stack();
 // starTrek.push('Kirk');
@@ -25,19 +28,51 @@ function is_palindrome(s) {
     stack1.push(s[i]);
   }
   // for (let i = s.length - 1; i >= 0; i--) {
-    for (let i = 0; i < s.length; i++) {
+  for (let i = 0; i < s.length; i++) {
     let topOftheStack = stack1.pop();
     if (topOftheStack !== s[i]) {
       return false;
     }
   }
-return true;
+  return true;
 }
 
-
 // true, true, true
-console.log(is_palindrome('dad'));
-console.log(is_palindrome('A man, a plan, a canal: Panama'));
-console.log(is_palindrome('1001'));
-console.log(is_palindrome('Tauhida'));
-console.log(is_palindrome('123'));
+// console.log(is_palindrome('dad'));
+// console.log(is_palindrome('A man, a plan, a canal: Panama'));
+// console.log(is_palindrome('1001'));
+// console.log(is_palindrome('Tauhida'));
+// console.log(is_palindrome('123'));
+
+//Matching parentheses in an expression
+/*
+  s = ()
+  loop through string, if open ( push to stack
+  if we see ) remove (
+  check if ( leftover in stack, to see if even or uneven
+*/
+function matchParantheses(s) {
+  const paranthesesStack = new Stack();
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      paranthesesStack.push(i); //pushes in the index
+      //console.log(paranthesesStack);
+    }
+    if (s[i] === ')') {
+      if (paranthesesStack.top === null) {
+        return `) at ${i}: Doesn't have an opening bracket `;
+      }
+      paranthesesStack.pop();
+    }
+  }
+
+  if (paranthesesStack.top !== null) {
+    return `Paranthesis at ${paranthesesStack.top.data} is not closed`;
+  }
+
+  return 'Valid';
+}
+
+// console.log(matchParantheses('(())'));
+// console.log(matchParantheses('(()'));
+// console.log(matchParantheses('())'));
