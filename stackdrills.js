c9f8402bed68c1cc7c5fed864b7c647eeaba79d5
Logan Wang <loganxwang@hotmail.com>
2018-12-14 08:42:50 -0800

    starting palindrome

--- stackdrills.js
+++ stackdrills.js
@@ -18,3 +18,13 @@ starTrek.pop();
 peek(starTrek);
 display(starTrek);
 
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  // your code goes here
+}
+
+// true, true, true
+console.log(is_palindrome('dad'));
+console.log(is_palindrome('A man, a plan, a canal: Panama'));
+console.log(is_palindrome('1001'));
+console.log(is_palindrome('Tauhida'));
