aaae3d342ad86e191a9cf8ca2d8866bf7d8205e9
mkcnly <mkcnly@gmail.com>
2018-12-14 09:40:06 -0800

    palindrome

--- stackdrills.js
+++ stackdrills.js
@@ -1,30 +1,47 @@
 'use strict';
 const Stack = require('./stack');
 
-function peek(stack) {
-  console.log(stack.top);
-}
-function display(stack) {
-  console.log(JSON.stringify(stack));
-}
+// function peek(stack) {
+//   console.log(stack.top);
+// }
+// function display(stack) {
+//   console.log(JSON.stringify(stack));
+// }
 
-const starTrek = new Stack();
-starTrek.push('Kirk');
-starTrek.push('Spock');
-starTrek.push('McCoy');
-starTrek.push('Scotty');
-starTrek.pop();
-starTrek.pop();
-peek(starTrek);
-display(starTrek);
+// const starTrek = new Stack();
+// starTrek.push('Kirk');
+// starTrek.push('Spock');
+// starTrek.push('McCoy');
+// starTrek.push('Scotty');
+// starTrek.pop();
+// starTrek.pop();
+// peek(starTrek);
+// display(starTrek);
 
 function is_palindrome(s) {
   s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-  // your code goes here
+  const stack1 = new Stack;
+  for (let i = 0; i < s.length; i++) {
+    stack1.push(s[i]);
+  }
+  console.log('test',JSON.stringify(stack1));
+  for (let i = s.length - 1; i >= 0; i--) {
+    let topOftheStack = stack1.pop();
+    console.log(topOftheStack)
+    if (topOftheStack !== s[i]) {
+      return false;
+    }
+    // else {
+    //   return true;
+    // }
+  }
+return true;
 }
 
+
 // true, true, true
-console.log(is_palindrome('dad'));
-console.log(is_palindrome('A man, a plan, a canal: Panama'));
-console.log(is_palindrome('1001'));
-console.log(is_palindrome('Tauhida'));
+// console.log(is_palindrome('dad'));
+// console.log(is_palindrome('A man, a plan, a canal: Panama'));
+// console.log(is_palindrome('1001'));
+// console.log(is_palindrome('Tauhida'));
+console.log(is_palindrome('123'));
