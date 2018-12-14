01676f9f3df0c3e7455ac83dc87aa86a84537385
mkcnly <mkcnly@gmail.com>
2018-12-14 11:20:38 -0800

    finished palindrome

--- stackdrills.js
+++ stackdrills.js
@@ -24,24 +24,20 @@ function is_palindrome(s) {
   for (let i = 0; i < s.length; i++) {
     stack1.push(s[i]);
   }
-  console.log('test',JSON.stringify(stack1));
-  for (let i = s.length - 1; i >= 0; i--) {
+  // for (let i = s.length - 1; i >= 0; i--) {
+    for (let i = 0; i < s.length; i++) {
     let topOftheStack = stack1.pop();
-    console.log(topOftheStack)
     if (topOftheStack !== s[i]) {
       return false;
     }
-    // else {
-    //   return true;
-    // }
   }
 return true;
 }
 
 
 // true, true, true
-// console.log(is_palindrome('dad'));
-// console.log(is_palindrome('A man, a plan, a canal: Panama'));
-// console.log(is_palindrome('1001'));
-// console.log(is_palindrome('Tauhida'));
+console.log(is_palindrome('dad'));
+console.log(is_palindrome('A man, a plan, a canal: Panama'));
+console.log(is_palindrome('1001'));
+console.log(is_palindrome('Tauhida'));
 console.log(is_palindrome('123'));
